b5784d2bf846327c33885f4ca32dcd9e7d8e3d87
Losalini <c.rokocakau@gmail.com>
2022-06-15 02:06:01 +1200

    Fix some eslint linter errors within local environment

--- script/local-storage.js
+++ script/local-storage.js
@@ -1,140 +1,142 @@
 // Variable declarations
-  const nameMobile1 = document.getElementById('user-name-mobile');
-  const nameDesktop1 = document.getElementById('user-name-desktop');
-  const emailMobile1 = document.getElementById('user-email-mobile');
-  const emailDesktop1 = document.getElementById('user-email-desktop');
-  const commentMobile1 = document.getElementById('user-comments-mobile');
-  const commentDesktop1 = document.getElementById('user-comments-desktop');
-  
-  // Variables to store the value of name, email & comment
-  let nameValueMobile1, emailValueMobile1, commentValueMobile1;
-  let nameValueDesktop1, emailValueDesktop1, commentValueDesktop1;
+const nameMobile1 = document.getElementById('user-name-mobile');
+const nameDesktop1 = document.getElementById('user-name-desktop');
+const emailMobile1 = document.getElementById('user-email-mobile');
+const emailDesktop1 = document.getElementById('user-email-desktop');
+const commentMobile1 = document.getElementById('user-comments-mobile');
+const commentDesktop1 = document.getElementById('user-comments-desktop');
 
-  // Object to store values/data from user input in the form
-  /* let userinputDataMobile = { 
-    username: nameValueMobile1, 
-    useremail: emailValueMobile1, 
+// Variables to store the value of name, email & comment
+let nameValueMobile1; let emailValueMobile1; let
+  commentValueMobile1;
+let nameValueDesktop1; let emailValueDesktop1; let
+  commentValueDesktop1;
+
+// Object to store values/data from user input in the form
+/* let userinputDataMobile = {
+    username: nameValueMobile1,
+    useremail: emailValueMobile1,
     usercomment: commentValueMobile1
   };
 
-  let userinputDataDesktop = { 
-    username: nameValueDesktop1, 
-    useremail: emailValueDesktop1, 
-    usercomment: commentValueDesktop1 
+  let userinputDataDesktop = {
+    username: nameValueDesktop1,
+    useremail: emailValueDesktop1,
+    usercomment: commentValueDesktop1
   }; */
-  
-  // Function Declarations
-  //Function to store form inputs in local storage
-  function saveToLocalStorage(userinputDataForScreen){
-    //Serialization of the JS object to JSON (conversion to a string) & storing it in local storage
-    let x = JSON.stringify(userinputDataForScreen);
-    localStorage.setItem('userData', x);
-  }
 
-  function prefillFormFields(userinputDataForScreen){
-    // Deserialization of the JSON string to a JS object & then getting user input from local storage
-    let storedInput = JSON.parse(localStorage.getItem('userData'));
+// Function Declarations
+// Function to store form inputs in local storage
+function saveToLocalStorage(userinputDataForScreen) {
+  // Serialization of the JS object to JSON (conversion to a string) & storing it in local storage
+  const x = JSON.stringify(userinputDataForScreen);
+  localStorage.setItem('userData', x);
+}
 
-    // Prefill mobile inputs in form
-    nameMobile1.setAttribute('value', storedInput.username);
-    emailMobile1.setAttribute('value', storedInput.useremail);
-    commentMobile1.innerHTML = storedInput.usercomment;
+function prefillFormFields(userinputDataForScreen) {
+  // Deserialization of the JSON string to a JS object & then getting user input from local storage
+  const storedInput = JSON.parse(localStorage.getItem('userData'));
 
-    // Prefill desktop inputs in form with the user data from mobile
-    nameDesktop1.setAttribute('value', storedInput.username);
-    emailDesktop1.setAttribute('value', storedInput.useremail);
-    commentDesktop1.innerHTML = storedInput.usercomment;
-  }
+  // Prefill mobile inputs in form
+  nameMobile1.setAttribute('value', storedInput.username);
+  emailMobile1.setAttribute('value', storedInput.useremail);
+  commentMobile1.innerHTML = storedInput.usercomment;
 
-  // If local storage is not empty, 
-  // then prefill the fields with the user data for mobile in local storage 
-  // when page is refreshed/reloaded   
-  /* if (userinputDataMobile || userinputDataDesktop){
+  // Prefill desktop inputs in form with the user data from mobile
+  nameDesktop1.setAttribute('value', storedInput.username);
+  emailDesktop1.setAttribute('value', storedInput.useremail);
+  commentDesktop1.innerHTML = storedInput.usercomment;
+}
+
+// If local storage is not empty,
+// then prefill the fields with the user data for mobile in local storage
+// when page is refreshed/reloaded
+/* if (userinputDataMobile || userinputDataDesktop){
     prefillFormFields(userinputDataMobile);
   } */
 
-  // For inputs in Mobile screen
-  nameMobile.addEventListener('change', (e) => {
-    // Accessing the name's value
-    nameValueMobile1 = nameMobile1.value;
+// For inputs in Mobile screen
+nameMobile.addEventListener('change', () => {
+  // Accessing the name's value
+  nameValueMobile1 = nameMobile1.value;
 
-    // Object to store values/data from user input in the form
-    userinputDataMobile = { 
-      username: nameValueMobile1, 
-      useremail: emailValueMobile1, 
-      usercomment: commentValueMobile1
-    };
+  // Object to store values/data from user input in the form
+  userinputDataMobile = {
+    username: nameValueMobile1,
+    useremail: emailValueMobile1,
+    usercomment: commentValueMobile1,
+  };
 
-    saveToLocalStorage(userinputDataMobile);
-  });
-  
-  emailMobile.addEventListener('change', (e) => {
-    // Accessing the email's value
-    emailValueMobile1 = emailMobile1.value;
+  saveToLocalStorage(userinputDataMobile);
+});
 
-    // Object to store values/data from user input in the form
-    userinputDataMobile = { 
-      username: nameValueMobile1, 
-      useremail: emailValueMobile1, 
-      usercomment: commentValueMobile1
-    };
+emailMobile.addEventListener('change', () => {
+  // Accessing the email's value
+  emailValueMobile1 = emailMobile1.value;
 
-    saveToLocalStorage(userinputDataMobile);
-  });
-  
-  commentMobile.addEventListener('change', (e) => {
-    // Accessing the comment's value
-    commentValueMobile1 = commentMobile1.value;
+  // Object to store values/data from user input in the form
+  userinputDataMobile = {
+    username: nameValueMobile1,
+    useremail: emailValueMobile1,
+    usercomment: commentValueMobile1,
+  };
 
-    // Object to store values/data from user input in the form
-    userinputDataMobile = { 
-      username: nameValueMobile1, 
-      useremail: emailValueMobile1, 
-      usercomment: commentValueMobile1
-    };
+  saveToLocalStorage(userinputDataMobile);
+});
 
-    saveToLocalStorage(userinputDataMobile);
-  });
-  
-  // For inputs in Desktop screen
-  nameDesktop.addEventListener('change', (e) => {
-    // Accessing the name's value
-    nameValueDesktop1 = nameDesktop1.value;
+commentMobile.addEventListener('change', () => {
+  // Accessing the comment's value
+  commentValueMobile1 = commentMobile1.value;
 
-    // Object to store values/data from user input in the form
-    userinputDataDesktop = { 
-      username: nameValueDesktop1, 
-      useremail: emailValueDesktop1, 
-      usercomment: commentValueDesktop1
-    };
+  // Object to store values/data from user input in the form
+  userinputDataMobile = {
+    username: nameValueMobile1,
+    useremail: emailValueMobile1,
+    usercomment: commentValueMobile1,
+  };
 
-    saveToLocalStorage(userinputDataDesktop);
-  });
-  
-  emailDesktop.addEventListener('change', (e) => {
-    // Accessing the email's value
-    emailValueDesktop1 = emailDesktop1.value;
-    
-    // Object to store values/data from user input in the form
-    userinputDataDesktop = { 
-      username: nameValueDesktop1, 
-      useremail: emailValueDesktop1, 
-      usercomment: commentValueDesktop1
-    };
-    
-    saveToLocalStorage(userinputDataDesktop);
-  });
-  
-  commentDesktop.addEventListener('change', (e) => {
-    // Accessing the comment's value
-    commentValueDesktop1 = commentDesktop1.value;
-    
-    // Object to store values/data from user input in the form
-    userinputDataDesktop = { 
-      username: nameValueDesktop1, 
-      useremail: emailValueDesktop1, 
-      usercomment: commentValueDesktop1
-    };
+  saveToLocalStorage(userinputDataMobile);
+});
 
-    saveToLocalStorage(userinputDataDesktop);
-  });
+// For inputs in Desktop screen
+nameDesktop.addEventListener('change', () => {
+  // Accessing the name's value
+  nameValueDesktop1 = nameDesktop1.value;
+
+  // Object to store values/data from user input in the form
+  userinputDataDesktop = {
+    username: nameValueDesktop1,
+    useremail: emailValueDesktop1,
+    usercomment: commentValueDesktop1,
+  };
+
+  saveToLocalStorage(userinputDataDesktop);
+});
+
+emailDesktop.addEventListener('change', () => {
+  // Accessing the email's value
+  emailValueDesktop1 = emailDesktop1.value;
+
+  // Object to store values/data from user input in the form
+  userinputDataDesktop = {
+    username: nameValueDesktop1,
+    useremail: emailValueDesktop1,
+    usercomment: commentValueDesktop1,
+  };
+
+  saveToLocalStorage(userinputDataDesktop);
+});
+
+commentDesktop.addEventListener('change', () => {
+  // Accessing the comment's value
+  commentValueDesktop1 = commentDesktop1.value;
+
+  // Object to store values/data from user input in the form
+  userinputDataDesktop = {
+    username: nameValueDesktop1,
+    useremail: emailValueDesktop1,
+    usercomment: commentValueDesktop1,
+  };
+
+  saveToLocalStorage(userinputDataDesktop);
+});

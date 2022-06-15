c1c95a15bedde26b93f8ea012984a072ad13b1c6
Losalini <c.rokocakau@gmail.com>
2022-06-15 21:26:50 +1200

    Refactor the prefillFormFields(storedInput) function

--- script/local-storage.js
+++ script/local-storage.js
@@ -1,4 +1,6 @@
 // Variable declarations
+const formMobile1 = document.getElementById('form-mobile');
+const formDesktop1 = document.getElementById('form-desktop');
 const nameMobile1 = document.getElementById('user-name-mobile');
 const nameDesktop1 = document.getElementById('user-name-desktop');
 const emailMobile1 = document.getElementById('user-email-mobile');
@@ -31,14 +33,11 @@ let userinputDataDesktop = {
 // Function to store form inputs in local storage
 function saveToLocalStorage(userinputDataForScreen) {
   // Serialization of the JS object to JSON (conversion to a string) & storing it in local storage
-  const x = JSON.stringify(userinputDataForScreen);
+  let x = JSON.stringify(userinputDataForScreen);
   localStorage.setItem('userData', x);
 }
 
-function prefillFormFields() {
-  // Deserialization of the JSON string to a JS object & then getting user input from local storage
-  const storedInput = JSON.parse(localStorage.getItem('userData'));
-
+function prefillFormFields(storedInput) {
   // Prefill mobile inputs in form
   nameMobile1.setAttribute('value', storedInput.username);
   emailMobile1.setAttribute('value', storedInput.useremail);
@@ -50,11 +49,14 @@ function prefillFormFields() {
   commentDesktop1.innerHTML = storedInput.usercomment;
 }
 
+// Deserialization of the JSON string to a JS object & then getting user input from local storage
+let storedInput = JSON.parse(localStorage.getItem('userData'));
+
 // If local storage is not empty,
 // then prefill the fields with the user data for mobile in local storage
 // when page is refreshed/reloaded
-if (userinputDataMobile || userinputDataDesktop) {
-  prefillFormFields();
+if (storedInput !== null) {
+  prefillFormFields(storedInput);
 }
 
 // For inputs in Mobile screen
